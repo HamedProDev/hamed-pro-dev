2df6e75df7ce483f47595693bc91e92711f69179
Hamed Hussein <hamedpro.work@gmail.com>
2026-07-27 21:15:28 +0200

    feat: remove Latest from the Blog from homepage

--- src/app/(public)/page.tsx
+++ src/app/(public)/page.tsx
@@ -1,7 +1,6 @@
 import { HeroSection } from '@/components/home/HeroSection'
 import { StatsBar } from '@/components/home/StatsBar'
 import { FeaturedProjects } from '@/components/home/FeaturedProjects'
-import { LatestPosts } from '@/components/home/LatestPosts'
 import { SkillsSection } from '@/components/home/SkillsSection'
 import { Testimonials } from '@/components/home/Testimonials'
 import { CTASection } from '@/components/home/CTASection'
@@ -14,7 +13,6 @@ export default function HomePage() {
       <StatsBar />
       <FeaturedProjects />
       <SkillsSection />
-      <LatestPosts />
       <Testimonials />
       <CTASection />
       <NewsletterSignup />
